be2e72faea1b5860ae1a56e05714041a93b6e3cf
Jakub Hoffman <jakubhoffman@eduroam-053-003.scc.uni-weimar.de>
2022-10-31 13:39:07 +0100

    Vegetation with BONUS finished

--- Assets/Scripts/VegetationGenerator.cs
+++ Assets/Scripts/VegetationGenerator.cs
@@ -1,9 +1,21 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class VegetationGenerator : MonoBehaviour
 {
+
+    [Serializable]
+    public struct vegetationPrefabsStruct
+    {
+        public GameObject prefab;
+        public int numObject;
+    }
+
+    [SerializeField]
+    private List<vegetationPrefabsStruct> prefabs = new List<vegetationPrefabsStruct>();
+
     public List<GameObject> vegetationPrefabs = new List<GameObject>();
 
     private List<GameObject> instances = new List<GameObject>();
@@ -18,6 +30,8 @@ public class VegetationGenerator : MonoBehaviour
 
     public bool reset = false;
 
+    
+
     // Start is called before the first frame update
     void Start()
     {
@@ -27,13 +41,26 @@ public class VegetationGenerator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // TODO: Exercise 1.2 -> 3.) 
+        // TODO: Exercise 1.2 -> 3.)
         // check & handle "reset" to regenerate vegetation instances
+
+        if (reset)
+        {
+            reset = false;
+            ClearVegetation();
+            GenerateVegetation();
+        }
     }
 
     void ClearVegetation()
     {
-        // TODO: part of Exercise 1.2 -> 3.)  
+        // TODO: part of Exercise 1.2 -> 3.)
+        for (int i = instances.Count; i > 0; i--)
+        {
+            GameObject toDestroy = instances[i - 1];
+            Destroy(toDestroy);
+            instances.Remove(instances[i-1]);
+        }
     }
 
     void GenerateVegetation()
@@ -42,22 +69,49 @@ public class VegetationGenerator : MonoBehaviour
         // Instantiate & transform random "vegetationPrefab"
 
         // your code here
-        for (int i = 0; i < numObjects; i++)
-        {
-            int someIndex = Random.Range(0, vegetationPrefabs.Count);
-            GameObject somePlant = vegetationPrefabs[someIndex];
-            Vector3 plantVector = new Vector3(
-                Random.Range(vegetationBoundsMin.x, vegetationBoundsMax.x),
-                0,
-                Random.Range(vegetationBoundsMin.z, vegetationBoundsMax.z)
-            );
-            Instantiate(somePlant, this.gameObject.transform);
 
-            somePlant.transform.position = plantVector;
-            somePlant.transform.rotation = new Quaternion(0, Random.Range(0, 360), 0, 0);
-            instances.Add(somePlant);
-            //to continue (nel)
+        // WITHOUT BONUS //
+
+        //for (int i = 0; i < numObjects; i++)
+        //{
+        //    int randomIndex = UnityEngine.Random.Range(0, vegetationPrefabs.Count);
+
+        //    GameObject somePlant = Instantiate(vegetationPrefabs[randomIndex], this.gameObject.transform);
+
+        //    somePlant.transform.position = new Vector3(
+        //        UnityEngine.Random.Range(vegetationBoundsMin.x, vegetationBoundsMax.x),
+        //        0,
+        //        UnityEngine.Random.Range(vegetationBoundsMin.z, vegetationBoundsMax.z)
+        //    );
+        //    somePlant.transform.rotation = new Quaternion(0, UnityEngine.Random.Range(0, 360), 0, 0);
+        //    instances.Add(somePlant);
+        //}
+
+        // WITH BONUS //
+        foreach(vegetationPrefabsStruct pre in prefabs)
+        {
+            for (int i = 0; i < pre.numObject; i++)
+            {
+
+                GameObject somePlant = Instantiate(
+                    pre.prefab,
+                    new Vector3(
+                        UnityEngine.Random.Range(vegetationBoundsMin.x, vegetationBoundsMax.x),
+                        0,
+                        UnityEngine.Random.Range(vegetationBoundsMin.z, vegetationBoundsMax.z)
+                    ),
+                    new Quaternion(
+                        0,
+                        UnityEngine.Random.Range(-1.0f, 1.0f),
+                        0,
+                        UnityEngine.Random.Range(-1.0f, 1.0f)
+                    ),
+                    this.gameObject.transform);
+
+                instances.Add(somePlant);
+            }
         }
+        
 
         // Collisions need to be resolved at a later time,
         // because Unity physics loop (Unity-internal evaluation of collisions)
@@ -75,14 +129,29 @@ public class VegetationGenerator : MonoBehaviour
 
         // your code here
 
+        for (int i = 0; i < instances.Count; i++)
+        {
+            Collider instanceCollider = instances[i].GetComponent<Collider>();
+            if (IsInRestrictedBounds(instanceCollider))
+            {
+                resolveAgain = true;
+                instances[i].transform.position = new Vector3(
+                    UnityEngine.Random.Range(vegetationBoundsMin.x, vegetationBoundsMax.x),
+                    0,
+                    UnityEngine.Random.Range(vegetationBoundsMin.z, vegetationBoundsMax.z)
+                );
+            }
+
+        }
         // resolve again (delayed), after new random transform applied to colliding instances
-        if (resolveAgain)
-            StartCoroutine(ResolveCollisions());
+        if (resolveAgain) StartCoroutine(ResolveCollisions());
     }
 
     bool IsInRestrictedBounds(Collider co)
     {
         // TODO: part of Exercise 1.2-> 2.)
-        return true;
+        Collider HouseCollider = GameObject.Find("House").GetComponent<Collider>();
+        if (co.bounds.Intersects(HouseCollider.bounds)) return true;
+        return false;
     }
 }
