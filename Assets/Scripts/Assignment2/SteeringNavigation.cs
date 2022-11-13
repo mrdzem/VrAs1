3ad110d8d4215234e9afdf754529245b43f1c998
Jakub Hoffman <jakubhoffman@Jakubs-MacBook-Pro.local>
2022-11-13 15:07:39 +0100

    up to 2.4 all done

--- Assets/Scripts/Assignment2/SteeringNavigation.cs
+++ Assets/Scripts/Assignment2/SteeringNavigation.cs
@@ -24,3 +24,18 @@ public class SteeringNavigation : MonoBehaviour
             transform.position += directionIndicator.forward * flyAction.action.ReadValue<float>() * Time.deltaTime * speedFactor;
     }
 }
+
+
+/*
+
+*Controller Directed
+Advantages:     Bigger flexibility, do not need to move body as much to pick a direction.
+Disadvantages:  If you accidently switch controllers you have no idea what is going on, you screwed man.
+
+*Gaze directed
+Advantages:     Feels more realistic and natural, like a fish.
+Disadvantages:  You cannot look around while walking because you switch direction. You can get dizzy more easily.
+
+*Conslusion:     We pick controller directed movement, because we like looking around while walking.
+
+*/
